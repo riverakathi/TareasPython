a1f62ad5013a3fbcb07ab43b078a4bb08d05332c
riverakathi <158237020+riverakathi@users.noreply.github.com>
2024-05-06 17:03:50 -0700

    Add files via upload

---
+++ piramide.py
@@ -0,0 +1,6 @@
+def main():
+    altura = int(input("Por favor, ingresa la altura del triángulo (número entero): "))
+    for i in range(1, altura + 1):
+        print("*" * i)
+if __name__ == "__main__":
+    main()
